3d0bf235c3033ca90b6dd817cb1ed677458f597b
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2024-01-20 08:27:21 +0100

    n

---
+++ app/Http/Controllers/API/PartController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Part;
+use Illuminate\Http\Request;
+
+class PartController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Part $part)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Part $part)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Part $part)
+    {
+        //
+    }
+}
