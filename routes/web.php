6a7f01d1ac3670d8f43e6808ebc2c60ce22ff857
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2024-03-13 08:52:59 +0100

    n

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', function () { return view('welcome'); });
+Route::get('/symlink', function () {return view('symlink');})->name('generate_symlink');
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
