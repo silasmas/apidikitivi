bd7bd997e8fd60e510e4769dede154b51704bcd7
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2024-04-01 07:56:35 +0100

    n

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -28,7 +28,7 @@ class Media extends Model
      */
     public function sessions()
     {
-        return $this->belongsToMany(Session::class)->withPivot('is_viewed');
+        return $this->belongsToMany(Session::class)->withPivot(['is_viewed', 'created_at']);
     }
 
     /**
