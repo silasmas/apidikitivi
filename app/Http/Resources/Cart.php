71aa34c2d66319ec3211c43e6112d73d9f3b813f
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2024-04-06 15:21:32 +0100

    Update Cart.php

--- app/Http/Resources/Cart.php
+++ app/Http/Resources/Cart.php
@@ -23,7 +23,7 @@ class Cart extends JsonResource
             'payment_code' => $this->payment_code,
             'type' => Type::make($this->type),
             'status' => Status::make($this->status),
-            'orders' => Order::collection($this->orders),
+            'orders' => Order::collection($this->orders)->unique('media_id')->all(),
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
             'user_id' => $this->user_id
