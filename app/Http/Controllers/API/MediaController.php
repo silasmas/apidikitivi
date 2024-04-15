4183a8480e77ad1085bcfb8296734f367068afa8
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2024-04-15 06:29:46 +0100

    Update MediaController.php

--- app/Http/Controllers/API/MediaController.php
+++ app/Http/Controllers/API/MediaController.php
@@ -145,8 +145,8 @@ class MediaController extends BaseController
         if ($request->hasHeader('X-user-id') AND $request->hasHeader('X-ip-address') OR $request->hasHeader('X-user-id') AND !$request->hasHeader('X-ip-address')) {
             $session = Session::where('user_id', $request->header('X-user-id'))->first();
 
-            dd($session);
             if (!empty($session)) {
+				dd($session->medias());
                 if ($session->medias() == null) {
                     $session->medias()->attach([$media->id]);
                 }
