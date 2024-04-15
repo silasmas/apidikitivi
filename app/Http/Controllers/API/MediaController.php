f86160eea8ee6d452266feacdeb6acedeef87df6
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2024-04-15 15:43:51 +0100

    Update MediaController.php

--- app/Http/Controllers/API/MediaController.php
+++ app/Http/Controllers/API/MediaController.php
@@ -539,8 +539,8 @@ class MediaController extends BaseController
         }
 
         $sessions = Session::whereHas('medias', function($query) {
-                            $query->where('media_session.is_viewed', 1)
-                                    ->orderByDesc('media_session.created_at');
+                            // $query->where('media_session.is_viewed', 1)
+                            $query->orderByDesc('media_session.created_at');
                         })->get();
 
         return $this->handleResponse(ResourcesSession::collection($sessions), __('notifications.find_all_sessions_success'));
