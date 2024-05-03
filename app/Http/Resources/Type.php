818038f399738f0aa93626b9653eeb644f6da431
silasmas <silasjmas@gmail.com>
2024-05-03 13:32:57 +0100

    ok

--- app/Http/Resources/Type.php
+++ app/Http/Resources/Type.php
@@ -22,6 +22,9 @@ class Type extends JsonResource
         return [
             'id' => $this->id,
             'type_name' => $this->type_name,
+            'type_name_fr' => $this->getTranslation('type_name', 'fr'),
+            'type_name_en' => $this->getTranslation('type_name', 'en'),
+            'type_name_ln' => $this->getTranslation('type_name', 'ln'),
             'type_description' => $this->type_description,
             'icon' => $this->icon,
             'color' => $this->color,
