935dba7aad2858372e773b563f293d697bf48bc5
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2024-05-20 14:47:57 +0100

    Update MediaController.php

--- app/Http/Controllers/API/MediaController.php
+++ app/Http/Controllers/API/MediaController.php
@@ -261,7 +261,7 @@ class MediaController extends BaseController
         ];
         $media = Media::find($id);
 
-        return $this->handleError($request->hasFile('cover_url'), __('validation.custom.type.required'), 400);
+        return $this->handleError($request->cover_url, __('validation.custom.type.required'), 400);
         // if ($inputs['media_title'] != null) {
         //     // Select all user medias to check unique constraint
         //     $medias = Media::where('user_id', $inputs['user_id'])->get();
