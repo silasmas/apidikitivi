2d9eb5659d5915cef00289f5ada30b9eabf83141
silasmas <silasjmas@gmail.com>
2024-07-10 13:23:50 +0100

    fpf

--- app/Http/Resources/Donation.php
+++ app/Http/Resources/Donation.php
@@ -27,5 +27,6 @@ class Donation extends JsonResource
             'created_at' => $this->created_at->format('Y-m-d H:i:s'),
             'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
         ];
+
     }
 }
